897490cff1532f2501cd211806c5c59f889b3312
anna-do <s2082066@ed.ac.uk>
2023-11-01 11:46:40 +0000

    Started implementing EuroMomo

--- Segmented regression.R
+++ Segmented regression.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(readxl)
 library(segmented)
 
+
 # SETUP ################################################
 
 engdat <- read_excel("Test data for segmented reg.xls",
@@ -69,30 +70,55 @@ print(EnglandTest)
 ggplot(engdat, aes(EndDate, ASR90,color=Sex)) + geom_point() + stat_smooth(method="lm") 
 
 
+
 ### Predicting #################################################
 
-# Data from the start of 2010 to the end of 2015
-start_year <- 2010
-end_year <- 2015
+# Data for fitting the model.
+fit_start <- 2005
+fit_end <- 2015
 
 # Predicting deaths for all of 2018 for females
-pred_year <- 2016
+pred_start <- 2016
+pred_end <- 2017
 sex_choice <- "Females"
 
 # fitdat is the data used to fit the model. fit is the fitted lm. segmod is the segmented model.
-fitdat <- engdat %>% filter(Sex == sex_choice, EndDate <= end_year, EndDate >= start_year)
+fitdat <- engdat %>% filter(Sex == sex_choice, EndDate <= fit_end, EndDate >= fit_start)
 fit <- lm(AllAgeDeaths ~ EndDate , data = fitdat)
 segmod <- segmented(fit)
 plot(segmod)
 
 # The years we are predicting for
-preddat <- data.frame(seq(pred_year, pred_year+1, by= 0.25))
+preddat <- data.frame(seq(pred_start, pred_end, by= 0.25))
 colnames(preddat) <- c("EndDate")
 
-predvals <- predict.segmented(segmod, newdata = preddat)
+predvals <- round(predict.segmented(segmod, newdata = preddat))
 dates <- preddat$EndDate
-truevals <- (engdat %>% filter(Sex==sex_choice, EndDate >= pred_year+1, EndDate <= pred_year+2))$AllAgeDeaths
+truevals <- (engdat %>% filter(Sex==sex_choice, EndDate >= pred_start, EndDate <= pred_end))$AllAgeDeaths
+difference <- abs(truevals - predvals)
 
-output <- cbind(dates, predvals, truevals)
+output <- cbind(dates, predvals, truevals, difference)
 output
 
+### Same thing as a function
+
+segreg <- function(fit_start, fit_end, pred_start, pred_end, sex_choice){
+  
+  fitdat <- engdat %>% filter(Sex == sex_choice, EndDate <= fit_end, EndDate >= fit_start)
+  fit <- lm(AllAgeDeaths ~ EndDate , data = fitdat)
+  segmod <- segmented(fit)
+  
+  preddat <- data.frame(seq(pred_start, pred_end, by= 0.25))
+  colnames(preddat) <- c("EndDate")
+  
+  predvals <- round(predict.segmented(segmod, newdata = preddat))
+  dates <- preddat$EndDate
+  truevals <- (engdat %>% filter(Sex==sex_choice, EndDate >= pred_start, EndDate <= pred_end))$AllAgeDeaths
+  difference <- abs(truevals - predvals)
+  
+  output <- cbind(dates, predvals, truevals, difference)
+  
+  return(output)
+}
+
+segreg(2005, 2015, 2016, 2017, "Females")
