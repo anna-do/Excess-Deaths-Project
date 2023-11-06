abaa8b8edfcba91ee3be64bdbe80b28a9a2ae2bf
anna-do <s2082066@ed.ac.uk>
2023-11-06 17:03:59 +0000

    finished seg reg

--- Segmented regression.R
+++ Segmented regression.R
@@ -1,7 +1,3 @@
-install.packages("tidyverse")
-install.packages("readxl")
-install.packages("segmented")
-
 library(tidyverse)
 library(readxl)
 library(segmented)
@@ -122,3 +118,86 @@ segreg <- function(fit_start, fit_end, pred_start, pred_end, sex_choice){
 }
 
 segreg(2005, 2015, 2016, 2017, "Females")
+
+
+
+
+# Setting up finalised dataset for write up ##########################################################
+
+# Loading dataset of deaths for England and Wales by week, by gender for 2013 to 2022
+dat <- read_excel("Deaths EngWal by week and gender 2013-2022.xlsx")
+
+# Creating a new dataset called NewData with the data grouped by quarter.
+NewData <- data.frame(matrix(ncol=5))
+colnames(NewData) <- c("Country", "Gender", "Year", "Quarter", "Deaths")
+
+for (i in 2012:2022){
+  for (j in c("All", "Female", "Male")){
+    thing <- dat %>% filter(Year == i, Gender == j, `Week number` <= 13)
+    NewData <- rbind(NewData, c("EngWal", j, i, 1, sum(thing$Deaths)))
+    thing <- dat %>% filter(Year == i, Gender == j, `Week number` <= 26, `Week number` >= 14)
+    NewData <- rbind(NewData, c("EngWal", j, i+0.25, 2, sum(thing$Deaths)))
+    thing <- dat %>% filter(Year == i, Gender == j, `Week number` <= 39, `Week number` >= 27)
+    NewData <- rbind(NewData, c("EngWal", j, i+0.5, 3, sum(thing$Deaths)))
+    thing <- dat %>% filter(Year == i, Gender == j, `Week number` <= 53, `Week number` >= 40)
+    NewData <- rbind(NewData, c("EngWal", j, i+0.75, 4, sum(thing$Deaths)))
+  }
+}
+
+NewData <- na.omit(NewData)
+
+# Creating a final dataset with the data in years overlapping by 9 months (ie 2021.25 is 
+# from start of April 2020 to end of March 2021)
+
+Data2 <- data.frame(matrix(ncol = 4))
+colnames(Data2) <- c("Country", "Gender", "EndDate", "Deaths")
+
+for (i in 2012:2021){
+  for (j in c("All", "Female", "Male")){
+    thing <- NewData %>% filter(Gender == j, Year <= i+1, Year >= i) 
+    Data2 <- rbind(Data2, c("EngWal", j, i+1, sum(as.numeric(thing$Deaths))))
+    print(sum(as.numeric(thing$Deaths)))
+    thing <- NewData %>% filter(Gender == j, Year <= i+1.25, Year >= i+0.25) 
+    Data2 <- rbind(Data2, c("EngWal", j, i+1.25, sum(as.numeric(thing$Deaths))))
+    print(sum(as.numeric(thing$Deaths)))
+    thing <- NewData %>% filter(Gender == j, Year <= i+1.5, Year >= i+0.5) 
+    Data2 <- rbind(Data2, c("EngWal", j, i+1.5, sum(as.numeric(thing$Deaths))))
+    print(sum(as.numeric(thing$Deaths)))
+    thing <- NewData %>% filter(Gender == j, Year <= i+1.75, Year >= i+0.75) 
+    Data2 <- rbind(Data2, c("EngWal", j, i+1.75, sum(as.numeric(thing$Deaths))))
+    print(sum(as.numeric(thing$Deaths)))
+  }
+}
+
+Data2 <- na.omit(Data2)
+Data2$EndDate <- as.numeric(Data2$EndDate)
+Data2$Deaths <- as.numeric(Data2$Deaths)
+
+# Sense checking
+ggplot(data = Data2, aes(x = EndDate, y = Deaths, fill=Gender)) + geom_point() + geom_smooth()
+
+
+segreg <- function(fit_start, fit_end, pred_start, pred_end, gender_choice){
+  
+  fitdat <- Data2 %>% filter(Gender == gender_choice, EndDate <= fit_end, EndDate >= fit_start)
+  fit <- lm(Deaths ~ EndDate, data = fitdat)
+  segmod <- segmented(fit)
+  
+  preddat <- data.frame(seq(pred_start, pred_end, by= 0.25))
+  colnames(preddat) <- c("EndDate")
+  
+  predvals <- round(predict.segmented(segmod, newdata = preddat))
+  dates <- preddat$EndDate
+  truevals <- (Data2 %>% filter(Gender==gender_choice, EndDate >= pred_start, EndDate <= pred_end))$Deaths
+  difference <- abs(truevals - predvals)
+  
+  output <- cbind(dates, predvals, truevals, difference)
+  
+  return(data.frame(output))
+}
+
+SegReg <- segreg(2013, 2017, 2018, 2022, "Female")
+
+SegReg %>% 
+  pivot_longer(cols = c("predvals", "truevals")) %>%
+  ggplot(aes(x = dates, y=value, fill=name)) + geom_point() + geom_smooth()
